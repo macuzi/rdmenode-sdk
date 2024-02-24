1c8b11a9f84836a4093f299e068a956ce81e9df8
lamberto <asghedom.lamberto@gmail.com>
2024-02-24 08:56:58 -0800

    added http methods + webhook route

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const readme = require("readmeio");
 
 const app = express();
-
 const port = process.env.PORT || 8000;
 
 require("dotenv").config();
@@ -13,13 +12,34 @@ if (!process.env.README_API_KEY) {
 }
 
 app.use((req, res, next) => {
-  readme.log(process.env.README_API_KEY, req, res, {
+  readme.log(process.env.README_API_KEY, req, res, groupFn, opts, {
     apiKey: "owlbert-api-key",
     label: "Owlbert",
     email: "owlbert@example.com",
   });
   next();
 });
+
+app.get("/", (req, res) => {
+  res.json({ message: " hello world" });
+});
+
+app.post("/", express.json(), (req, res) => {
+  res.status(200).send();
+});
+
+app.post(
+  "/webhook",
+  express.json({ type: "application/json" }),
+  (async) => (req, res) => {
+    const signature = req.headers["readme-signature"];
+    try {
+      readme.verifyWebhook(req.body, signature, process.env.SECRET);
+    } catch (e) {
+      return res.status(401).json({ error: e.message });
+    }
+  }
+);
 
 const server = app.listen(port, "0.0.0.0", () => {
   console.log(
