b71f7d64eca6a192c5e24f5a31364e2f57099e0f
lamberto <asghedom.lamberto@gmail.com>
2024-02-23 23:26:45 -0800

    middleware function

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+const readme = require("readmeio");
 
+require("dotenv").config();
+
+const app = express();
+
+if (!process.env.README_API_KEY) {
+  console.error("Missing `README_API_KEY` enviorment variable");
+  process.exit(1);
+}
+
+app.use((req, res, next) => {
+  readme.log(process.env.README_API_KEY, req, res, {
+    apiKey: "owlbert-api-key",
+    label: "Owlbert",
+    email: "owlbert@example.com",
+  });
+  next();
+});
